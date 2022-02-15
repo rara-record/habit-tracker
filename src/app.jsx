908628c7ed991091f57673652b71c08c122aca0b
bora <a52447879@gamil.com>
2022-02-16 05:45:25 +0900

    상태업데이트까지

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,13 @@
 import './app.css';
+import React, { Component } from 'react'
 import Habits from './components/habits';
 
-function App() {
-  return (
-    <Habits />
-  );
+class App extends Component {
+  render() {
+    return (
+      <Habits />
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -19,13 +19,22 @@ export class Habit extends Component {
       <li className='habit'>
         <span className="habit-name">{name}</span>
         <span className="habit-count">{count}</span>
-        <button className='habit-button habit-increase' onClick={this.handleIncrement}>
+        <button 
+          className='habit-button habit-increase' 
+          onClick={this.handleIncrement}
+        >
           <i className="fas fa-square-plus"></i>
         </button>
-        <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
+        <button 
+          className='habit-button habit-decrease' 
+          onClick={this.handleDecrement}
+        >
           <i className="fas fa-square-minus"></i>
         </button>
-        <button className='habit-button habit-delete' onClick={this.handleDelete}>
+        <button 
+          className='habit-button habit-delete' 
+          onClick={this.handleDelete}
+        >
           <i className="fas fa-trash"></i>
         </button>
       </li>
