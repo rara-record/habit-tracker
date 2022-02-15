0100054f965907b61c25cc5d8f9a9fcad641bb00
bora <a52447879@gamil.com>
2022-02-16 04:55:31 +0900

    props로 받은 콜백함수 호출

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
 
 export class Habit extends Component {
-  state = {
-    count: 0,
-  };
-
   handleIncrement = () => {
-    // state 오브젝트 안에 있는 count를 증가 한뒤, statr를 업데이트 해야함.
-    this.setState({ count: this.state.count +1});
+    this.props.onIncrement(this.props.habit)
   }
 
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit)
+  }
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit)
   }
 
   render() {
@@ -27,7 +25,7 @@ export class Habit extends Component {
         <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
           <i className="fas fa-square-minus"></i>
         </button>
-        <button className='habit-button habit-delete'>
+        <button className='habit-button habit-delete' onClick={this.handleDelete}>
           <i className="fas fa-trash"></i>
         </button>
       </li>
