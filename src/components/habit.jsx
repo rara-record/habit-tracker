c10b6cd7885015aae856a365c7f42776f957b184
bora <a52447879@gamil.com>
2022-02-15 16:42:18 +0900

    증가, 감소 버튼 만들기: state를 변경할때는 setState를 호출하자.

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 
 export class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count +1});
+  }
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count -1});
+  }
+
   render() {
     return (
       <li className='habit'>
         <span className="habit-name">Reading</span>
-        <span className="habit-count">8</span>
-        <button className='habit-button habit-increase'>
+        <span className="habit-count">{this.state.count}</span>
+        <button className='habit-button habit-increase' onClick={this.handleIncrement}>
           <i className="fas fa-square-plus"></i>
         </button>
-        <button className='habit-button habit-decrease'>
+        <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
           <i className="fas fa-square-minus"></i>
         </button>
         <button className='habit-button habit-delete'>
