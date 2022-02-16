cf852be00c46842190a7b46fe7fe1dd0174fdddd
bora <a52447879@gamil.com>
2022-02-16 20:37:19 +0900

    폼 컴포넌트 생성/ 습관 추가 기능

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class HabitAddForm extends Component {
+  formRef = React.createRef();
+  inputRef = React.createRef();
+
+  onSubmit = event => {
+    event.preventDefault();
+    // input에 value를 받아서, value가 있다면 onAdd에 value를 담아서 호출함.
+    const name = this.inputRef.current.value;
+    name && this.props.onAdd(name);
+    this.formRef.current.reset(); // this.inputRef.current.value = '';
+  };
+
+  render() {
+    return (
+      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
+        <input
+          ref={this.inputRef}
+          type="text"
+          className="add-input"
+          placeholder="습관을 입력하세요"
+        />
+        <button className="add-button">Add</button>
+      </form>
+    );
+  }
+}
+
+export default HabitAddForm;
