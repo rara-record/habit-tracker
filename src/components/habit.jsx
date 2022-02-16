04c2109e5f20febb6aeba2a5032039833e69b6c4
bora <a52447879@gamil.com>
2022-02-16 20:40:41 +0900

    Ref까지완료

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,45 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export class Habit extends Component {
+class Habit extends Component {
   handleIncrement = () => {
-    this.props.onIncrement(this.props.habit)
-  }
+    this.props.onIncrement(this.props.habit);
+  };
 
   handleDecrement = () => {
-    this.props.onDecrement(this.props.habit)
-  }
+    this.props.onDecrement(this.props.habit);
+  };
 
   handleDelete = () => {
-    this.props.onDelete(this.props.habit)
-  }
+    this.props.onDelete(this.props.habit);
+  };
 
   render() {
-    const { name, count } = this.props.habit
+    const { name, count } = this.props.habit;
     return (
-      <li className='habit'>
+      <li className="habit">
         <span className="habit-name">{name}</span>
         <span className="habit-count">{count}</span>
-        <button 
-          className='habit-button habit-increase' 
+        <button
+          className="habit-button habit-increase"
           onClick={this.handleIncrement}
         >
           <i className="fas fa-square-plus"></i>
         </button>
-        <button 
-          className='habit-button habit-decrease' 
+        <button
+          className="habit-button habit-decrease"
           onClick={this.handleDecrement}
         >
           <i className="fas fa-square-minus"></i>
         </button>
-        <button 
-          className='habit-button habit-delete' 
+        <button
+          className="habit-button habit-delete"
           onClick={this.handleDelete}
         >
           <i className="fas fa-trash"></i>
         </button>
       </li>
-    )
+    );
   }
 }
 
-export default Habit
+export default Habit;
